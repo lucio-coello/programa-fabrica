8c756bfdef4bd2db1cd594c486a4d56fa39b1ee4
lucio-coello <lordbukake042@gmail.com>
2025-05-21 15:32:34 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
+//main.c
 #include <stdio.h>
 #include "funciones.h"
 
 int main() {
+    int opcion; 
+
     do {
         printf("\n--- MENU ---\n");
         printf("1. Agregar producto\n");
@@ -10,17 +13,38 @@ int main() {
         printf("4. Verificar produccion\n");
         printf("5. Salir\n");
         printf("Opcion: ");
-        scanf("%d", &opcion);
+
+        if (scanf("%d", &opcion) != 1) {
+            printf("Entrada invalida. Por favor ingrese un numero.\n");
+            while (getchar() != '\n'); 
+            continue;
+        }
 
         switch (opcion) {
-            case 1: agregarProducto(); break;
-            case 2: editarProducto(); break;
-            case 3: eliminarProducto(); break;
-            case 4: calcularTotales(); break;
-            case 5: printf("Saliendo...\n"); break;
-            default: printf("Opcion invalida.\n");
+            case 1:
+                agregarProducto();
+                break;
+            case 2:
+                editarProducto();
+                break;
+            case 3:
+                eliminarProducto();
+                break;
+            case 4:
+                calcularTotales(); 
+                break;
+            case 5:
+                printf("Saliendo...\n");
+                break;
+            default:
+                printf("Opcion invalida.\n");
         }
+
+        while (getchar() != '\n');
+
     } while (opcion != 5);
 
     return 0;
 }
+
+//fin del main.c
